114c49017553ee78be73a91b12a9cc098e3639d3
racCC <32578834+racCC@users.noreply.github.com>
2023-10-02 22:42:58 +0530

    Update MapSave.java

--- src/main/java/mapSave/MapSave.java
+++ src/main/java/mapSave/MapSave.java
@@ -3,72 +3,118 @@ import entity.Continent;
 import entity.Country;
 import entity.RiskMap;
 
-import java.io.PrintWriter;
-import java.util.Set;
+import java.io.IOException;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.List;
 
-
-
 /**
- * The class  will save the data in map file when the user creates a map
+ * The class MapSave will save the data in map file when the user creates a map
  *
  * @author Rachit
  */
 
-public class SaveMap {
-
-    private RiskMap riskMap;
+public class MapSave {
     /**
-     * Create a neighbor list as a string for the countries
+     * This method writes the map details to map file
      *
-     * @param p_Neighbors the country to add to neighbor list
-     * @return String result of neighbor list
+     * @param p_map  object of the map which is being processed.
+     * @param p_file object of file to store map data.
      */
-    public String createANeighborList(Set<Country> p_Neighbors) {
-        String l_result = "";
-        for (Country l_Neighbor : p_Neighbors) {
-            l_result += l_Neighbor.getName() + " ";
+
+    public static void saveMapFile(RiskMap p_map, File p_file) {
+
+        FileWriter l_fileWriter;
+        try {
+            if (p_map == null) {
+                System.out.println("Map object is NULL! ");
+            }
+
+            String l_content = parseMapAndReturnString(p_map);
+            l_fileWriter = new FileWriter(p_file, false);
+            l_fileWriter.write(l_content);
+            l_fileWriter.close();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
         }
-        return l_result.length() > 0 ? l_result.substring(0, l_result.length() - 1) : "";
     }
+
     /**
-     * Save map into file as continent and country
+     * This method processes the map by calling three different methods and makes a string to be written in map file.
      *
-     * @param name name of file
-     * @param p_GameMap parameter GameMap class
-     * @return boolean true if written
+     * @param p_Map object of the map which is processed
+     * @return String to be written in the map file
      */
+    private static String parseMapAndReturnString(RiskMap p_Map) {
 
-    public boolean saveMapIntoFile(RiskMap p_GameMap, String name) {
-        String mapData = "[Map]\n\n[Continents]\n";
-        List<Continent> continents = p_GameMap.getContinentIds();
-        int numberOfContinents = continents.size();
+        return String.valueOf(processContinent(p_Map)) +
+                processCountries(p_Map) +
+                processAdjacentCountries(p_Map);
+    }
 
-        for (int i = 0; i < numberOfContinents; i++) {
-            Continent continent = continents.get(i);
-            mapData += continent.getName() + " " + continent.getControlValue();
-            mapData += "\n";
+
+    /**
+     * This method processes the continents.
+     *
+     * @param p_Map object of the map which is being processed
+     * @return a string that contains details of the continents that will eventually be written in the map file.
+     */
+    private static StringBuilder processContinent(RiskMap p_Map) {
+        StringBuilder l_continentData = new StringBuilder();
+        l_continentData.append("[continents]");
+        l_continentData.append("\n");
+        for (Continent l_continent : p_Map.getContinentIds()) {
+            l_continentData.append(l_continent.getName()).append(" ").append(l_continent.getControlValue());
+            l_continentData.append("\n");
         }
+        return l_continentData;
+    }
 
-        mapData += "[Territories]\n";
-        for (Continent continent : p_GameMap.getContinentIds()) {
-            for (Country country : p_GameMap.getCountryIds()) {
-                mapData += country.getName() + " " + country.getDId() + " " + (country.getBorders()) + "\n";
+    /**
+     * This method is for processing countries.
+     *
+     * @param p_Map object of the map that is being processed
+     * @return a string that contains details of countries that will ultimately be written in the map file.
+     */
+    private static StringBuilder processCountries(RiskMap p_Map) {
+        StringBuilder l_countryData = new StringBuilder();
+        l_countryData.append("\n");
+        l_countryData.append("[countries]");
+        l_countryData.append("\n");
+
+        for (Continent l_continent : p_Map.getContinentIds()) {
+            for (Country l_country : l_continent.getId()) {
+                l_countryData.append(l_country.getDId()).append(" ").append(l_country.getName()).append(" ").append(l_continent.getId());
+                l_countryData.append("\n");
             }
-
-            PrintWriter writeData = null;
-            try {
-                writeData = new PrintWriter("testMap.map/" + name + ".map");
-                writeData.println(mapData);
-                return true;
-            } catch (Exception ex) {
-                return false;
-            } finally {
-                writeData.close();
-            }
-
         }
-        return true;
+        return l_countryData;
+    }
 
+    /**
+     * This method is for processing adjacent countries.
+     *
+     * @param p_Map object of map that is being processed.
+     * @return a string that contains adjacent countries and that will be written in map file.
+     */
+    private static StringBuilder processAdjacentCountries(RiskMap p_Map) {
+        StringBuilder l_countryData = new StringBuilder();
+        l_countryData.append("\n");
+        l_countryData.append("[borders]");
+        l_countryData.append("\n");
+        for (Continent l_continent : p_Map.getContinentIds()) {
+            List<Country> l_countryList = l_continent.getId();
+            if (l_countryList != null) {
+                for (Country l_country : l_countryList) {
+                    l_countryData.append(l_country.getDId());
+                    for (Country l_adjacentCountries : l_country.getBorders()) {
+                        l_countryData.append(" ");
+                        l_countryData.append(l_adjacentCountries.getDId());
+                    }
+                    l_countryData.append("\n");
+                }
+            }
+        }
+        return l_countryData;
     }
 }
