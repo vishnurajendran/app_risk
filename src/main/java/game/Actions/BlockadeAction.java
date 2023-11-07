e42295a86ea8c5a29a44f3a9aa336249ccfe879a
HWJFish <wwc641810324@gmail.com>
2023-11-06 22:04:11 -0500

    Add update for Action execstatus when excution fail

--- src/main/java/game/Actions/BlockadeAction.java
+++ src/main/java/game/Actions/BlockadeAction.java
@@ -18,26 +18,32 @@ public class BlockadeAction extends GameAction {
 
     @Override
     public void execute(Command p_cmd) {
+        d_execStatus = ActionExecStatus.Success;
         Player l_player=d_context.getCurrentPlayer();
         RiskMap l_riskMap=d_context.getEngine().getMap();
         //not 100% sure if cmd is handled correctly here
         ArrayList<String> l_arguments=p_cmd.getCmdAttributes().get(0).getArguments();
         if(l_arguments.size()!=1){
             System.out.print("Incorrect amount of arguments for blockade action.");
+            d_execStatus=ActionExecStatus.Fail;
+            return;
         }
         int l_countryID=Integer.parseInt(l_arguments.get(0));
         if(l_player.isCardAvailable(CardType.Blockade)){
             System.out.println("You don't have blockade card to blockade.");
+            d_execStatus=ActionExecStatus.Fail;
             return;
         }
         Country l_country=l_riskMap.getCountryById(l_countryID);
         if(isNull(l_country)){
             System.out.println("Country ID is invalid.");
+            d_execStatus=ActionExecStatus.Fail;
             return;
         }
 
         if(l_player.isCountryOwned(l_country)){
             System.out.println("The country is not owned by you.");
+            d_execStatus=ActionExecStatus.Fail;
             return;
         }
 
