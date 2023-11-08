ad4c56af36847f4ea64f7b54c57a7598340ae30f
Soham-2411 <52789205+Soham-2411@users.noreply.github.com>
2023-11-08 15:13:45 -0500

    [WIP] Action Test functions

--- src/test/java/game/Actions/AdvanceActionTest.java
+++ src/test/java/game/Actions/AdvanceActionTest.java
@@ -18,20 +18,25 @@ class AdvanceActionTest {
     ArrayList<Player> d_gamePlayersTest;
     private final PrintStream standardOut = System.out;
     private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    Continent d_continent;
+    GameAction d_gameActionTest;
+    PlayerHandler d_playerHandlerTest;
 
     @BeforeEach
     void setUp(){
         d_gameEngineTest = new GameEngine();
         d_gameEngineTest.initialise();
         System.setOut(new PrintStream(outputStreamCaptor));
-//        System.setOut(new PrintStream(outputStreamCaptor));
-//        d_advanceActionTest = new AdvanceAction();
-//        PlayerHandler.addGamePlayers(new ArrayList<>(Arrays.asList("player1", "player2", "player3", "player4")), null);
-//        Continent continent = new Continent(1, "test-continent", 3);
-//        d_gamePlayersTest = PlayerHandler.getGamePlayers();
-//        for (int i = 0; i < d_gamePlayersTest.size(); i++) {
-//            d_gamePlayersTest.get(i).assignCountry(new Country(i, "test-country" + i, 1), 0);
-//        }
+        d_advanceActionTest = new AdvanceAction();
+        PlayerHandler.addGamePlayers(new ArrayList<>(Arrays.asList("player1", "player2", "player3", "player4")), null);
+        d_continent = new Continent(1, "test-continent", 3);
+        d_gamePlayersTest = PlayerHandler.getGamePlayers();
+        for (int i = 1; i <= d_gamePlayersTest.size(); i++) {
+            d_gameEngineTest.submitCommand(Command.parseString("loadmap testresources/wow.map"));
+            d_gamePlayersTest.get(i-1).assignCountry(d_gameEngineTest.getMap().getCountryById(i), 5);
+        }
+        d_gameActionTest = GameActionFactory.getAdvanceAction();
+        d_gameActionTest.SetContext(new Context(d_gamePlayersTest.get(1), d_gameEngineTest));
     }
 
     @AfterEach
@@ -43,29 +48,34 @@ class AdvanceActionTest {
 
     @Test
     void TestCommandValidity(){
-        d_advanceActionTest = new AdvanceAction();
-        PlayerHandler.addGamePlayers(new ArrayList<>(Arrays.asList("player1", "player2", "player3", "player4")), null);
-        Continent continent = new Continent(1, "test-continent", 3);
-        d_gamePlayersTest = PlayerHandler.getGamePlayers();
-        for (int i = 1; i < d_gamePlayersTest.size(); i++) {
-            d_gameEngineTest.submitCommand(Command.parseString("loadmap testresources/wow.map"));
-            d_gamePlayersTest.get(i-1).assignCountry(d_gameEngineTest.getMap().getCountryById(i), 0);
-        }
-        GameAction l_gameActionTest = GameActionFactory.getAdvanceAction();
-        l_gameActionTest.SetContext(new Context(d_gamePlayersTest.get(1), d_gameEngineTest));
-        Command l_cmd = Command.parseString("advance 1 2");
-        l_gameActionTest.execute(l_cmd);
+
+        Command l_cmd = Command.parseString("advance 2 3");
+        d_gameActionTest.execute(l_cmd);
         // To check if advance command is valid, should create an error
         Assertions.assertEquals(GameCommands.ADVANCE_ERROR_MESSAGES.get(0), outputStreamCaptor.toString().trim());
 
-        l_cmd = Command.parseString("advance 1 2 3");
-        l_gameActionTest.execute(l_cmd);
-        // To check if player2 owns the country 1, which should return false;
+    }
+
+    @Test
+    void TestCountryAdjacency(){
+        Command l_cmd = Command.parseString("advance 2 3 3");
+        d_gameActionTest.execute(l_cmd);
+        Assertions.assertEquals(GameCommands.ADVANCE_ERROR_MESSAGES.get(3), outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    void TestCountryOwnership(){
+        Command l_cmd = Command.parseString("advance 1 3 3");
+        d_gameActionTest.execute(l_cmd);
         Assertions.assertEquals(GameCommands.ADVANCE_ERROR_MESSAGES.get(1), outputStreamCaptor.toString().trim());
     }
 
-    void TestCountryOwnership(){
+    @Test
+    void TestCountryNegotiated(){
+        Command l_cmd = Command.parseString("advance 1 3 3");
 
+        d_gameActionTest.execute(l_cmd);
+        Assertions.assertEquals(GameCommands.ADVANCE_ERROR_MESSAGES.get(1), outputStreamCaptor.toString().trim());
     }
 
 
