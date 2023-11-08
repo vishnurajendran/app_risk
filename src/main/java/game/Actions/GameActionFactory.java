1738132cd254477f36b592a18867d0a20ddc37b9
Vishnu Rajendran <rajendran.vishnu97@gmail.com>
2023-11-07 22:59:15 -0500

    hopefully this is final merge conflict related issues.

--- src/main/java/game/Actions/GameActionFactory.java
+++ src/main/java/game/Actions/GameActionFactory.java
@@ -5,19 +5,73 @@ package game.Actions;
  */
 public class GameActionFactory {
 
+    /**
+     * private constructor to block instancing.
+     */
     private GameActionFactory(){
 
     }
 
+    /**
+     * @return new instance of AddRemovePlayerAction
+     */
     public static GameAction getUpdatePlayerAction(){
         return new AddRemovePlayerAction();
     }
 
+    /**
+     * @return new instance of AssignCountriesAction
+     */
     public static GameAction getAssignCountriesAction(){
         return new AssignCountriesAction();
     }
 
+    /**
+     * @return new instance of AirliftAciton
+     */
+    public static GameAction getAirliftAction() {
+        return new AirliftAction();
+    }
+
+    /**
+     * @return new instance of AdvanceAction
+     */
+    public static GameAction getAdvanceAction() {
+        return new AdvanceAction();
+    }
+
+    /**
+     * @return new instance of BombAction
+     */
+    public static GameAction getBombAction() {
+        return new BombAction();
+    }
+
+    /**
+     * @return new instance of BlockadeAction
+     */
+    public static GameAction getBlockadeAction() {
+        return new BlockadeAction();
+    }
+
+    /**
+     * @return new instance of CommitAction
+     */
+    public static GameAction getCommitAction() {
+        return new CommitAction();
+    }
+
+    /**
+     * @return new instance of DeployAction
+     */
     public static GameAction getDeployAction() {
         return new DeployAction();
+    }
+
+    /**
+     * @return new instance of NegotiateAction
+     */
+    public static GameAction getNegotiateAction() {
+        return new NegotiateAction();
     }
 }
