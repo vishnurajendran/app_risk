7042374049fddda6b692fe4a7ae2773f52025513
racCC <32578834+racCC@users.noreply.github.com>
2023-11-07 20:35:43 -0500

    Added javadoc for BombOrder and minor refactoring

--- src/main/java/game/Orders/BombOrder.java
+++ src/main/java/game/Orders/BombOrder.java
@@ -1,49 +1,71 @@
 package game.Orders;
+
 import entity.Country;
 import entity.Player;
 import entity.RiskMap;
 
-
 /**
- * Bomb order class used to execute the bomb order.
+ * The BombOrder class represents an order to execute a bomb action in the Risk game.
+ * It allows a player to reduce the number of armies in a target country by half, provided
+ * the target country is not owned by the current player. This class extends the Order class.
+ *
+ * @see Order
  */
 public class BombOrder extends Order {
 
-    public BombOrder(){
-        d_ctxPlayer = null;
-        d_targetCountry = 0;
-        d_sourceCountry = 0;
-        d_riskMap = null;
+    private Player d_CtxPlayer;
+    private int d_TargetCountry;
+    private RiskMap d_RiskMap;
+
+    /**
+     * Default constructor for the {@code BombOrder} class.
+     * Initializes the context player, target country ID and the game's Risk map to null.
+     */
+    public BombOrder() {
+        d_CtxPlayer = null;
+        d_TargetCountry = 0;
+        d_RiskMap = null;
     }
 
-
-    public BombOrder(Player p_player, int p_targetCountry, RiskMap p_riskMap) {
-        d_ctxPlayer = p_player;
-        d_targetCountry = p_targetCountry;
-        d_riskMap = p_riskMap;
+    /**
+     * Constructor for the {@code BombOrder} class.
+     *
+     * @param p_Player       The player who is issuing the bomb order.
+     * @param p_TargetCountry The ID of the target country to be bombed.
+     * @param p_RiskMap      The RiskMap representing the game map.
+     */
+    public BombOrder(Player p_Player, int p_TargetCountry, RiskMap p_RiskMap) {
+        d_CtxPlayer = p_Player;
+        d_TargetCountry = p_TargetCountry;
+        d_RiskMap = p_RiskMap;
     }
 
+    /**
+     * Executes the bomb order by reducing the number of armies in the target country by half.
+     * The action is executed if the current player does not own the target country and
+     * the target country has more than 1 army.
+     * If the action is successful, the game state is updated, and a message is printed.
+     */
     @Override
     public void executeOrder() {
         // Get the current player
-        Player currentPlayer = d_ctxPlayer;
+        Player l_CurrentPlayer = d_CtxPlayer;
 
         // Get the target country by its ID from the game's map
-        Country targetCountry = d_riskMap.getCountryById(d_targetCountry);
+        Country l_TargetCountry = d_RiskMap.getCountryById(d_TargetCountry);
 
         // Check if the current player can perform the bomb action based on the game rules
-        if (currentPlayer != null && targetCountry != null) {
+        if (l_CurrentPlayer != null && l_TargetCountry != null) {
             // Check if the target country is not owned by the current player
-            if (!currentPlayer.isCountryOwned(targetCountry)) {
-                // Reduce the number of armies in the target country
-                int armiesInTargetCountry = targetCountry.getArmy();
-                if (armiesInTargetCountry > 1) {
-                    // You can define your own logic for how many armies are reduced in the bomb action.
-                    int armiesToReduce = Math.min(armiesInTargetCountry - 1, 3); // Example: Reduce up to 3 armies
-                    targetCountry.setArmy(armiesInTargetCountry - armiesToReduce);
+            if (!l_CurrentPlayer.isCountryOwned(l_TargetCountry)) {
+                // Reduce the number of armies in the target country by half
+                int l_ArmiesInTargetCountry = l_TargetCountry.getArmy();
+                if (l_ArmiesInTargetCountry > 1) {
+                    int l_ArmiesToReduce = l_ArmiesInTargetCountry / 2; // Reduce by half
+                    l_TargetCountry.setArmy(l_ArmiesInTargetCountry - l_ArmiesToReduce);
 
-                    // Update the game state accordingly
-                    System.out.println("Bomb action executed: " + armiesToReduce + " armies reduced in " + targetCountry.getName());
+                    // Updating the game state accordingly
+                    System.out.println("Bomb action executed: " + l_ArmiesToReduce + " armies reduced in " + l_TargetCountry.getName());
                 } else {
                     System.out.println("Bomb action failed: The target country doesn't have enough armies.");
                 }
@@ -56,5 +78,3 @@ public class BombOrder extends Order {
         }
     }
 }
-
-
