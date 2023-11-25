5c22765b1299f7c494ed35159bf1e5cce11506c1
HWJFish <wwc641810324@gmail.com>
2023-11-25 07:03:36 -0500

    refactor map save to use adapter TODO: finish ConquestMapWriter and add user input for file writers

--- src/main/java/entity/MapLoaderAdapter.java
+++ src/main/java/entity/MapLoaderAdapter.java
@@ -1,13 +1,25 @@
 package entity;
 
+/**
+ * Adapter class for map loader
+ */
 public class MapLoaderAdapter {
     private static MapReader d_mapReader;
 
+    /**
+     * Constructor for adapter, MapReader should be one type of map reader
+     * @param p_mapReader The map reader needed
+     */
     public MapLoaderAdapter(MapReader p_mapReader){
         d_mapReader =p_mapReader;
     }
 
 
+    /**
+     * Load the map using the map reader and adapt to riskMap
+     * @param p_mapName
+     * @return
+     */
     public RiskMap loadMap(String p_mapName){
         return d_mapReader.readMap(p_mapName);
 
