9fb603d60f2306b63fca4bdc82937da498114e44
Soham Sakaria <52789205+Soham-2411@users.noreply.github.com>
2023-11-29 08:34:30 +0530

    Update AddRemovePlayerAction.java
    removed rogue error message for invalid command

--- src/main/java/game/Actions/AddRemovePlayerAction.java
+++ src/main/java/game/Actions/AddRemovePlayerAction.java
@@ -32,6 +32,7 @@ public class AddRemovePlayerAction extends GameAction{
                 if(d_CmdOption.size() > i+1 && d_CmdArguments.size() > i+1){
                     if(d_CmdOption.get(i+1).equals(GameCommands.CMD_GAME_PLAYER_OPTION_ADD_STRATEGIES) && d_CmdArguments.get(i).size() == d_CmdArguments.get(i+1).size()){
                         PlayerHandler.addGamePlayers(d_CmdArguments.get(i), d_CmdArguments.get(i+1), l_engine.getMap());
+                        i++;
                     } else{
                         d_execStatus = ActionExecStatus.Fail;
                         System.out.println("ERROR: Invalid Strategy command");
