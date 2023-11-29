484e282aed5f829b1a4719d5c044a5b47c72b7b9
racCC <32578834+racCC@users.noreply.github.com>
2023-11-29 18:58:42 -0500

    java doc for AggressiveStrategyTest

--- src/test/java/game/States/Strategy/AggressiveStrategyTest.java
+++ src/test/java/game/States/Strategy/AggressiveStrategyTest.java
@@ -1,3 +1,9 @@
+/**
+ * Unit test for the AggressiveStrategy class.
+ * It includes test methods to validate the behavior of the AggressiveStrategy class.
+ *
+ * @author Rachit
+ */
 package game.States.Strategy;
 
 import common.Command;
@@ -20,8 +26,13 @@ class AggressiveStrategyTest {
     GameEngine d_gameEngineTest;
     ArrayList<Player> d_gamePlayersTest;
 
+    /**
+     * Sets up the test environment before each test method is executed.
+     * Initializes a GameEngine, loads a map, and adds players with specific strategies.
+     * Assigns countries to players for testing.
+     */
     @BeforeEach
-    void setup(){
+    void setup() {
         d_gameEngineTest = new GameEngine();
         d_gameEngineTest.initialise();
         d_gameEngineTest.submitCommand(Command.parseString("loadmap testResources/WoW.map"));
@@ -29,7 +40,7 @@ class AggressiveStrategyTest {
         ArrayList<String> p_strategiesToAdd = new ArrayList<>(Arrays.asList("a", "h"));
         PlayerHandler.addGamePlayers(p_playersToAdd, p_strategiesToAdd, d_gameEngineTest.getMap());
         d_gamePlayersTest = PlayerHandler.getGamePlayers();
-        for(Player player: d_gamePlayersTest){
+        for (Player player : d_gamePlayersTest) {
             player.setStrategyContext(d_gameEngineTest);
         }
         d_gamePlayersTest.get(0).assignCountry(d_gameEngineTest.getMap().getCountryById(27), 4);
@@ -38,29 +49,41 @@ class AggressiveStrategyTest {
         d_gamePlayersTest.get(1).assignCountry(d_gameEngineTest.getMap().getCountryById(38), 2);
     }
 
+    /**
+     * Cleans up the test environment after each test method is executed.
+     * Quits the game, shuts down the GameEngine, and sets GameEngine instance to null.
+     */
     @AfterEach
-    void cleanup(){
+    void cleanup() {
         d_gameEngineTest.quitGame();
         d_gameEngineTest.shutdown();
         d_gameEngineTest = null;
     }
 
+    /**
+     * Tests the findStrongestCountry method of the AggressiveStrategy class.
+     * Verifies that the method correctly identifies the strongest country owned by a player.
+     */
     @Test
-    void FindStrongestCountryTest(){
+    void FindStrongestCountryTest() {
         AggressiveStrategy l_aggressiveStrat = new AggressiveStrategy();
         Country findStrongest = l_aggressiveStrat.findStrongestCountry(d_gamePlayersTest.get(0).getCountriesOwned());
         assertEquals(findStrongest, d_gamePlayersTest.get(0).getCountriesOwned().get(0));
     }
 
+    /**
+     * Tests the deploy phase of the AggressiveStrategy class.
+     * Verifies that the strategy deploys reinforcements to the strongest country owned by the player.
+     */
     @Test
-    void AggressiveStrategyDeployTest(){
+    void AggressiveStrategyDeployTest() {
         Player l_currentPlayer = d_gamePlayersTest.get(0);
         l_currentPlayer.assignReinforcementsToPlayer();
         AggressiveStrategy l_aggressiveStrat = new AggressiveStrategy();
         Country findStrongest = l_aggressiveStrat.findStrongestCountry(l_currentPlayer.getCountriesOwned());
         assertEquals(findStrongest, l_currentPlayer.getCountriesOwned().get(0));
         l_currentPlayer.issueOrder();
-        if(l_currentPlayer.hasOrders()){
+        if (l_currentPlayer.hasOrders()) {
             Order order = l_currentPlayer.nextOrder();
             System.out.println("Running: " + order);
             order.executeOrder();
@@ -68,14 +91,19 @@ class AggressiveStrategyTest {
         assertEquals(l_currentPlayer.getCountriesOwned().get(0).getArmy(), 7);
     }
 
+    /**
+     * Tests the advance phase of the AggressiveStrategy class.
+     * Verifies that the strategy issues an order to advance
+     * to a neighboring country after deploying reinforcements.
+     */
     @Test
-    void AggressiveStrategyAdvanceTest(){
+    void AggressiveStrategyAdvanceTest() {
         Player l_currentPlayer = d_gamePlayersTest.get(0);
         AggressiveStrategy l_aggressiveStrat = new AggressiveStrategy();
         Country findStrongest = l_aggressiveStrat.findStrongestCountry(l_currentPlayer.getCountriesOwned());
         assertEquals(findStrongest, l_currentPlayer.getCountriesOwned().get(0));
         l_currentPlayer.issueOrder();
-        if(l_currentPlayer.hasOrders()){
+        if (l_currentPlayer.hasOrders()) {
             Order order = l_currentPlayer.nextOrder();
             System.out.println("Running: " + order);
             order.executeOrder();
