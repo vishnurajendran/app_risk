e8252b88f2a8677dd91d5002d8ffb554c3e6c3e1
racCC <32578834+racCC@users.noreply.github.com>
2023-11-29 13:59:10 -0500

    using strategy data and minor corrections in AggressiveStrategy.java WIP

--- src/main/java/game/States/Strategy/AggressiveStrategy.java
+++ src/main/java/game/States/Strategy/AggressiveStrategy.java
@@ -2,10 +2,12 @@ package game.States.Strategy;
 
 import entity.Country;
 import entity.PlayerHandler;
+import game.Data.StrategyData;
 import game.Orders.AdvanceOrder;
 import game.Orders.DeployOrder;
 import game.Orders.Order;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,13 +16,25 @@ import java.util.List;
  */
 public class AggressiveStrategy extends Strategy {
 
+    private StrategyData d_strategyData;
+
+    /**
+     * Constructor for AggressiveStrategy.
+     *
+     * @param strategyData The StrategyData instance to be used by this strategy.
+     */
+    public AggressiveStrategy(StrategyData strategyData) {
+        this.d_strategyData = strategyData;
+    }
+
     /**
      * @return an order based on the strategy rules
      */
     @Override
     public Order decide() {
-        if (d_strategyData.getCurrentPlayer() == null) {
-            // Handle the case where the current player is null.
+        // Ensure StrategyData is available
+        if (d_strategyData == null || d_strategyData.getCurrentPlayer() == null) {
+            // Handle the case where the current player or StrategyData is null.
             return null;
         }
 
@@ -38,13 +52,23 @@ public class AggressiveStrategy extends Strategy {
             AdvanceOrder advanceOrder = attackWithStrongestCountry(strongestCountry);
 
             // Return a compound order containing both deploy and advance orders
-            return new CompoundOrder(deployOrder, advanceOrder);
+            List<Order> orders = new ArrayList<>();
+            orders.add(deployOrder);
+            orders.add(advanceOrder);
+
+            return (Order) orders;
         } else {
             // If no strong country found, do nothing
             return null;
         }
     }
 
+    /**
+     * Finds the strongest country among the given list of countries based on the number of armies.
+     *
+     * @param countries The list of countries to search.
+     * @return The strongest country, or null if the list is empty.
+     */
     private Country findStrongestCountry(List<Country> countries) {
         if (countries.isEmpty()) {
             return null;
@@ -61,14 +85,24 @@ public class AggressiveStrategy extends Strategy {
         return strongestCountry;
     }
 
-
+    /**
+     * Calculates the number of armies to deploy in the strongest country.
+     *
+     * @param strongestCountry The strongest country to reinforce.
+     * @return The number of armies to deploy.
+     */
     private int calculateArmiesToDeploy(Country strongestCountry) {
         // You can implement your own logic to determine the number of armies to deploy.
         // For simplicity, let's deploy a fixed number of armies, e.g., 3.
         return 3;
     }
 
-
+    /**
+     * Creates an AdvanceOrder to attack with the strongest country.
+     *
+     * @param strongestCountry The strongest country to launch an attack from.
+     * @return The AdvanceOrder representing the attack.
+     */
     private AdvanceOrder attackWithStrongestCountry(Country strongestCountry) {
         // You need to implement logic here to determine the target country for the attack.
         // For simplicity, let's assume you are attacking the first neighboring country for now.
