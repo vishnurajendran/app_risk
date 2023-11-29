fd76620f93d184731f9da45c55ef287e20929f71
racCC <32578834+racCC@users.noreply.github.com>
2023-11-29 13:55:25 -0500

    Created AggressiveStrategy.java WIP

--- src/main/java/game/States/Strategy/AggressiveStrategy.java
+++ src/main/java/game/States/Strategy/AggressiveStrategy.java
@@ -1,44 +1,48 @@
 package game.States.Strategy;
 
+import entity.Country;
+import entity.PlayerHandler;
 import game.Orders.AdvanceOrder;
 import game.Orders.DeployOrder;
 import game.Orders.Order;
-import entity.Country;
-import entity.PlayerHandler;
-import game.Data.StrategyData;
 
-import java.util.Collections;
 import java.util.List;
 
+/**
+ * Represents an Aggressive strategy in the APP Risk game.
+ * This strategy focuses on centralization of forces and attacking strategically.
+ */
 public class AggressiveStrategy extends Strategy {
 
-    private StrategyData d_strategyData;
-
-    public AggressiveStrategy(StrategyData strategyData) {
-        this.d_strategyData = strategyData;
-    }
-
+    /**
+     * @return an order based on the strategy rules
+     */
     @Override
     public Order decide() {
-        List<Country> playerCountries = d_strategyData.getCurrentPlayer().getCountriesOwned();
-
-        // Check if the current player has any armies
-        if (d_strategyData.getCurrentPlayer().getAvailableReinforcements() == 0) {
-            // If no armies, mark the player as committed and return null
-            PlayerHandler.markCommitted(d_strategyData.getCurrentPlayer());
+        if (d_strategyData.getCurrentPlayer() == null) {
+            // Handle the case where the current player is null.
             return null;
         }
 
-        // Deploy on the strongest country
-        Country strongestCountry = findStrongestCountry(playerCountries);
-        DeployOrder deployOrder = calculateArmiesToDeploy(strongestCountry);
+        PlayerHandler.markComitted(d_strategyData.getCurrentPlayer());
+        System.out.println("Committing orders for: " + d_strategyData.getCurrentPlayer().getPlayerName());
 
-        // Advance to attack a neighboring country not owned by the player
-        AdvanceOrder advanceOrder = calculateAdvanceOrder(strongestCountry);
+        Country strongestCountry = findStrongestCountry(d_strategyData.getCurrentPlayer().getCountriesOwned());
 
-        // Return the sequence of order
-        return new OrderSequence(deployOrder, advanceOrder, new MarkCommittedOrder(d_strategyData.getCurrentPlayer())) {
-        };
+        if (strongestCountry != null) {
+            // Deploy armies in the strongest country
+            int armiesToDeploy = calculateArmiesToDeploy(strongestCountry);
+            DeployOrder deployOrder = new DeployOrder(d_strategyData.getCurrentPlayer(), armiesToDeploy, strongestCountry.getDId(), d_strategyData.getEngine().getMap());
+
+            // Attack with the strongest country
+            AdvanceOrder advanceOrder = attackWithStrongestCountry(strongestCountry);
+
+            // Return a compound order containing both deploy and advance orders
+            return new CompoundOrder(deployOrder, advanceOrder);
+        } else {
+            // If no strong country found, do nothing
+            return null;
+        }
     }
 
     private Country findStrongestCountry(List<Country> countries) {
@@ -46,41 +50,35 @@ public class AggressiveStrategy extends Strategy {
             return null;
         }
 
-        // Sort countries by armies in descending order
-        Collections.sort(countries, Collections.reverseOrder());
+        Country strongestCountry = countries.get(0);
 
-        // Return the strongest country
-        return countries.get(0);
-    }
-
-    private DeployOrder calculateArmiesToDeploy(Country strongestCountry) {
-        int armiesToDeploy = d_strategyData.getCurrentPlayer().getAvailableReinforcements();
-        return new DeployOrder(d_strategyData.getCurrentPlayer(), armiesToDeploy, strongestCountry, d_strategyData.getEngine().getMap());
-    }
-
-    private AdvanceOrder calculateAdvanceOrder(Country strongestCountry) {
-        // Find a neighboring country not owned by the player
-        Country targetCountry = findTargetCountry(strongestCountry);
-
-        if (targetCountry != null) {
-            // Advance from the strongest country to the target country
-            return new AdvanceOrder(d_strategyData.getCurrentPlayer(), strongestCountry, targetCountry, d_strategyData.getEngine().getMap());
-        } else {
-            // If no valid target, return null
-            return null;
-        }
-    }
-
-    private Country findTargetCountry(Country sourceCountry) {
-        // Find a neighboring country not owned by the player
-        List<Country> neighbors = d_strategyData.getEngine().getMap().ge(sourceCountry);
-
-        for (Country neighbor : neighbors) {
-            if (!neighbor.isOwnedBy(d_strategyData.getCurrentPlayer())) {
-                return neighbor;
+        for (Country country : countries) {
+            if (country.getArmy() > strongestCountry.getArmy()) {
+                strongestCountry = country;
             }
         }
 
-        return null;
+        return strongestCountry;
+    }
+
+
+    private int calculateArmiesToDeploy(Country strongestCountry) {
+        // You can implement your own logic to determine the number of armies to deploy.
+        // For simplicity, let's deploy a fixed number of armies, e.g., 3.
+        return 3;
+    }
+
+
+    private AdvanceOrder attackWithStrongestCountry(Country strongestCountry) {
+        // You need to implement logic here to determine the target country for the attack.
+        // For simplicity, let's assume you are attacking the first neighboring country for now.
+        List<Country> neighboringCountries = strongestCountry.isNeighbour();
+        if (!neighboringCountries.isEmpty()) {
+            Country targetCountry = neighboringCountries.get(0);
+            return new AdvanceOrder(d_strategyData.getCurrentPlayer(), strongestCountry.getDId(), targetCountry.getDId(), 1, d_strategyData.getEngine().getMap());
+        } else {
+            // If no neighboring countries, do nothing
+            return null;
+        }
     }
 }
